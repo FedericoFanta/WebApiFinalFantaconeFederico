8762e637173f6593f600213675cf162d6e9bb288
Federico Fantacone <fede26f@hotmail.com>
2022-11-18 15:59:19 -0300

    Agregar archivos de proyecto.

---
+++ WebApiFinalFantaconeFederico/Controllers/HospitalController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using WebApiFinalFantaconeFederico.Data;
+using WebApiFinalFantaconeFederico.Models;
+
+namespace WebApiFinalFantaconeFederico.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HospitalController : ControllerBase
+    {
+        public HospitalContext Context { get; set; }    //Generar propiedad con nuestra clase de DATA
+
+        public HospitalController(HospitalContext context)   //Generar constructor Con nuesta clase de DATA
+        {
+            this.Context = context;
+        }
+
+
+
+        [HttpGet]
+        public List<Hospital> Get()
+        {
+            //EF
+            List<Hospital> hospitales = Context.hospitales.ToList(); 
+
+            return hospitales; // retornamos la lista 
+        }
+
+
+        [HttpGet("{Num_Cama}")]
+        public IEnumerable<Hospital> Get(int num_cama)
+        {
+
+           
+            
+
+
+                var hospital = (from h in Context.hospitales
+                                where h.Num_Cama > num_cama
+                                select h).ToList();
+
+                return (IEnumerable<Hospital>)hospital;
+            
+
+            
+        }
+
+
+
+    }
+}
---
+++ WebApiFinalFantaconeFederico/Data/HospitalContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiFinalFantaconeFederico.Models;
+
+namespace WebApiFinalFantaconeFederico.Data
+{
+    public class HospitalContext:DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options) : base(options) { }
+
+
+        public DbSet<Doctor> Doctores { get; set; }
+        public DbSet<Hospital> hospitales { get; set; } 
+    }
+}
